6e7ec286bacf3c425df04e105aee7d97551b2fa8
Andrew-InTheBox <andrew.box.81@gmail.com>
2026-07-23 18:37:01 -0500

    2 cores is good

--- start_autoMods.bat
+++ start_autoMods.bat
@@ -25,7 +25,7 @@ set "serverConfig=serverDZ.cfg"
 ::Server profile folder
 set "serverProfile=config"
 ::Logical CPU cores to use (Equal or less than available)
-set "serverCPU=4"
+set "serverCPU=2"
 
 :: SteamCMD configuration (required for auto-updating mods)
 :: Override any of these in .env for machine-specific config
